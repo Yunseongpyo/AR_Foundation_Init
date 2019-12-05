cdd6b4b7a9d9422c762c3a559c392738f4a3a649
Yunseongpyo <53991007+Yunseongpyo@users.noreply.github.com>
2019-12-05 21:58:57 +0900

    1205_PortFoilo_OnlyClick

--- Assets/02.Scripts/ARTapTopPlaceObject.cs
+++ Assets/02.Scripts/ARTapTopPlaceObject.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-//using UnityEngine.XR.ARSubsystems;
 using System;
 
 public class ARTapTopPlaceObject : MonoBehaviour
 {
+
+
     private ARRaycastManager rayManager;
     private GameObject visual;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    
+
+    private ObjectsSpawner checkbool;
+
+
+    public Camera arCamera;
+    private RaycastHit hitobj;
     private void Start()
     {
         rayManager = FindObjectOfType<ARRaycastManager>();
@@ -22,16 +28,26 @@ public class ARTapTopPlaceObject : MonoBehaviour
 
     private void Update()
     {
+        //Touch touch = Input.GetTouch(0);
+        //Ray cameraHit = arCamera.ScreenPointToRay(touch.position);
         rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
         if(hits.Count >0)
         {
             transform.position = hits[0].pose.position;
             transform.rotation = hits[0].pose.rotation;
 
-            if(!visual.activeInHierarchy)
+            if (!visual.activeInHierarchy)
             {
                 visual.SetActive(true);
             }
         }
+        //if (Physics.Raycast(cameraHit,out hitobj))
+        //{
+        //    if(hitobj.transform.CompareTag("CLEAR") && checkbool.ischeckClear == true)
+        //    {
+        //        visual.SetActive(true);
+        //        checkbool.ischeckClear = false;
+        //    }
+        //}
     }
 }
---
+++ Assets/02.Scripts/ObjectsSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsSpawner : MonoBehaviour
+{
+    public GameObject objectToSpwan;
+    private ARTapTopPlaceObject placementIndicator;
+
+    public bool ischeckClear;
+    private void Start()
+    {
+        placementIndicator = FindObjectOfType<ARTapTopPlaceObject>();
+
+    }
+    private void Update()
+    {
+        if(Input.touchCount >0 && Input.touches[0].phase == TouchPhase.Began  /*&& !ischeckClear*/)
+        {
+            GameObject obj = Instantiate(objectToSpwan, placementIndicator.transform.position, placementIndicator.transform.rotation);
+            placementIndicator.gameObject.SetActive(false);
+            //ischeckClear = !ischeckClear;
+        }
+    }
+}
