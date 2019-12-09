3cb2f714c78da0437417e949f1576de0fe033d6e
Yunseongpyo <53991007+Yunseongpyo@users.noreply.github.com>
2019-12-09 13:40:34 +0900

    1209_SpeechToText_errorAdd

---
+++ Assets/02.Scripts/SpeechToTextController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechToTextController : MonoBehaviour
+{
+    public IBM.Watsson.Examples.ExampleStreaming voice;
+    public bool onOffRecording;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!onOffRecording)
+        {
+            voice.Active = false; 
+        }
+        else if(onOffRecording)
+        {
+            voice.Active = true;
+        }
+    }
+}
